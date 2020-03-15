056de4a7705677e3b94c4944275fd3dbc8e34e44
Tony Minseok Kim <tonyminseokkim11@gmail.com>
2020-03-15 16:53:46 -0700

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name' : 'Tony Minseok Kim',
+    'birthday': '961119',
+    'gender': 'Male',
+    'job': 'College Student'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name' : 'Yeji Lim',
+    'birthday': '980228',
+    'gender': 'Female',
+    'job': 'Accountant'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name' : 'Lyfe Haard',
+    'birthday': '990131',
+    'gender': 'Female',
+    'job': 'Programmer'
+  }
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develope management system!</h2>
+    <div>
+      {
+        customers.map(customer => {
+          return (
+            <Customer
+              key={customer.id}
+              id={customer.id}
+              image={customer.image}
+              name={customer.name}
+              birthday={customer.birthday}
+              gender={customer.gender}
+              job={customer.job}
+            /> 
+          );
+        })
+      }
     </div>
   );
 }
